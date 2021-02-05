6c08e3310191e8f053eb15f9262dee3091f9ec23
Angel Jovanov <angel@jovanov.me>
2021-02-05 13:17:14 +0100

    Init

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,14 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/coolshoeshine/ckko4y7pz6d5517qi9rmzzwt9', // stylesheet location
+	center: campground.geometry.coordinates, // starting position [lng, lat]
+	zoom: 10 // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker()
+	.setLngLat(campground.geometry.coordinates)
+	.setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
+	.addTo(map);
